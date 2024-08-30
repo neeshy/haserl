0983d30aeb15b5c5013f53b931d944b26aff8add
neeshy <neeshy@tfwno.gf>
2024-08-30 14:10:08 -0400

    Redefine print() to buffer its output
    A table named BUFFER is used for the purpose. The contents of BUFFER are
    concatenated together and written to stdout upon the completion of the
    script's execution.

--- common.c
+++ common.c
@@ -74,25 +74,76 @@ lua_init(void)
 void
 lua_set(const char *tbl, const char *key, size_t key_size, const char *value, size_t value_size)
 {
-	lua_getglobal(global.L, tbl);
-	if (!lua_istable(global.L, -1)) {
-		lua_pop(global.L, 1);
-		lua_newtable(global.L);
-		lua_setglobal(global.L, tbl);
-		lua_getglobal(global.L, tbl);
+	lua_State *L = global.L;
+
+	lua_getglobal(L, tbl);
+	if (!lua_istable(L, -1)) {
+		lua_pop(L, 1);
+		lua_newtable(L);
+		lua_pushvalue(L, -1);
+		lua_setglobal(L, tbl);
 	}
 
-	lua_pushlstring(global.L, key, key_size);
-	lua_pushlstring(global.L, value, value_size);
-	lua_settable(global.L, -3);
-	lua_pop(global.L, 1);
+	lua_pushlstring(L, key, key_size);
+	lua_pushlstring(L, value, value_size);
+	lua_settable(L, -3);
+	lua_pop(L, 1);
+}
+
+static int
+lua_print(lua_State *L)
+{
+	int n = lua_gettop(L);
+
+	lua_getglobal(L, "BUFFER");
+	lua_pushinteger(L, lua_objlen(L, -1) + 1);
+	lua_insert(L, 1);
+	lua_insert(L, 1);
+
+	lua_getglobal(L, "string");
+	lua_getfield(L, -1, "format");
+	lua_remove(L, -2);
+	lua_insert(L, -n - 1);
+	lua_call(L, n, 1);
+
+	lua_settable(L, -3);
+
+	return 0;
 }
 
 void
 lua_doscript(const char *filename)
 {
-	if (luaL_loadfile(global.L, filename) || lua_pcall(global.L, 0, 0, 0)) {
-		die("%s", lua_tostring(global.L, -1));
+	lua_State *L = global.L;
+
+	lua_newtable(L);
+	lua_setglobal(L, "BUFFER");
+
+	lua_pushcfunction(L, lua_print);
+	lua_setglobal(L, "print");
+
+	if (luaL_loadfile(L, filename) || lua_pcall(L, 0, 0, 0)) {
+		die("%s", lua_tostring(L, -1));
+	}
+
+	lua_getglobal(L, "table");
+	lua_getfield(L, -1, "concat");
+	lua_remove(L, -2);
+	lua_getglobal(L, "BUFFER");
+	if (lua_pcall(L, 1, 1, 0)) {
+		die("%s", lua_tostring(L, -1));
+	}
+
+	size_t len;
+	const char *buffer = lua_tolstring(L, -1, &len);
+	if (!buffer) {
+		die("lua_tolstring: BUFFER not accessible");
+	}
+
+	ssize_t n = write(1, buffer, len);
+	lua_pop(L, 1);
+	if (n != len || n == -1) {
+		die_status(errno, "write: %s", strerror(errno));
 	}
 }
 
